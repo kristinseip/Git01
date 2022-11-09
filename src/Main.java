457f2827428e4aced8af80d0957907c9b5d39b0e
KristinSeipStubbe <kristin.seip.stubbe@gmail.com>
2022-11-09 11:54:01 +0100

    branch

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
-
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world! Change 2");
-    System.out.println("first change and third");
-    System.out.println("I want other text here");
-      System.out.println("here is another change, this is really fun");
+        System.out.println("first change and third");
+        System.out.println("I want other text here");
+        System.out.println("here is another change, this is really fun");
         System.out.println("testing merging");
         System.out.println("Let's have a party");
         System.out.println("this is another attempt");
@@ -14,6 +14,6 @@ public class Main {
         System.out.println("What will happen here?");
         System.out.println("Will I be able to merge now?");
 
+        System.out.println("is this a new branch?");
 
-    }
-}
+    }}
