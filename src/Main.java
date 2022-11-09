830fd1f4a82b49ac51cc73f104f1bfdd012ed1cd
KristinSeipStubbe <kristin.seip.stubbe@gmail.com>
2022-11-09 10:27:55 +0100

    new attempt

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("testing merging");
         System.out.println("Let's have a party");
         System.out.println("this is another attempt");
-
+        System.out.println("i want this to work");
         System.out.println("What will happen here?");
         System.out.println("Will I be able to merge now?");
     }
